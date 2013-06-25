bb44f33ac35e996f4cad360f71f5440d23aed5ef
Beneil Eisavi <Beneil.Eisavi@yahoo.com>
2013-06-25 01:18:24 -0700

    update

--- demo/apps/home/urls.py
+++ demo/apps/home/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('demo.apps.home.views',
 	url(r'^$','index_view',name='vista_principal'),
 	url(r'^about/$','about_view',name='vista_about'),
+	url(r'^productions/$','productions_view',name='vista_productions'),
 )
